3b94d89aa0d215951a5a3a2032782c36da8b3976
Brandon Boudrias <bt.boudrias@gmail.com>
2016-08-01 13:29:36 -0700

    script for exercise3

---
+++ exercise3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+name1, name2, name3 = ('Jim', 'Jack', 'Jill')
+out1 = "{:>10} {:>10} {:>10}".format(name1, name2, name3)
+print out1
+
+name4 = raw_input('Fourth Name: ')
+out2 = name4
+print out2

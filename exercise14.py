f0135f25aada5f4cf644ac1e37d111fe7196e900
Brandon Boudrias <bt.boudrias@gmail.com>
2016-08-02 10:02:32 -0700

    exercise14 tweaks

--- exercise14.py
+++ exercise14.py
@@ -9,11 +9,16 @@ network_device['model'] = 'EX-ChalupaSupreme'
 
 for param, value in network_device.items():
     print '{:<10} is {:<10}'.format(param, value)
+else:
+    print
 
 network_device['password'] = 'flibble1'
+network_device['secret'] = 'keepItSafe'
 
 for param, value in network_device.items():
     print '{:<10} is {:<10}'.format(param, value)
+else:
+    print
 
 
 print network_device.get('device_type', 'cisco_ios')

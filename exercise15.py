8f69c5e8748e36e8f758d8fb3ed3769b52002a4f
Brandon Boudrias <bt.boudrias@gmail.com>
2016-08-02 10:42:23 -0700

    exercise15

---
+++ exercise15.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+network_device = {}
+network_device['ip_address'] = '10.0.0.1'
+network_device['username'] = 'brandune'
+network_device['password'] = 'flibble'
+network_device['vendor'] = 'Junaristcoiquitiulus'
+network_device['model'] = 'EX-ChalupaSupreme'
+
+for param, value in network_device.items():
+    print '{:<10} is {:<10}'.format(param, value)
+else:
+    print
+
+network_device['password'] = 'flibble1'
+network_device['secret'] = 'keepItSafe'
+
+for param, value in network_device.items():
+    print '{:<10} is {:<10}'.format(param, value)
+else:
+    print
+
+try:
+    print network_device['device_type']
+except KeyError:
+    print 'cisco_ios'

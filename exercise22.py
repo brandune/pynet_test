4c8addf9d804bb8426aa240784bc277c7df7da22
Brandon Boudrias <bt.boudrias@gmail.com>
2016-08-03 09:36:51 -0700

    exercise22

---
+++ exercise22.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+def func1():
+    print "I'm function one."
+
+def func2():
+    print "I'm function two."
+
+def func3():
+    print "I'm function three."
+
+def main():
+    func1()
+    func2()
+    func3()
+
+if __name__ == '__main__':
+    main()
